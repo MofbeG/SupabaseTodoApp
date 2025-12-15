9f71e86302916bf87c67a1fb4a8bf6fb78d5f012
Mihail <zone.man.2017@mail.ru>
2025-12-15 18:53:20 +0600

    Добавлен SupabaseConfig

---
+++ app/src/main/java/com/sigma/supabasetodoapp/network/SupabaseConfig.java
@@ -0,0 +1,14 @@
+package com.sigma.supabasetodoapp.network;
+
+public class SupabaseConfig {
+    public static final String SUPABASE_URL = "https://XXXX.supabase.co";
+    public static final String SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+
+    public static final String AUTH_SIGNUP_URL =
+            SUPABASE_URL + "/auth/v1/signup";
+    public static final String AUTH_SIGNIN_URL =
+            SUPABASE_URL + "/auth/v1/token?grant_type=password";
+
+    public static final String TABLE_URL =
+            SUPABASE_URL + "/rest/v1/tasks";
+}
